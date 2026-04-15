692f02746363f97507815d38262f4716db906847
Rohit Kumar Tiwari <rohittiwari0403@gmail.com>
2026-04-15 11:51:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Arithmetic Operators/arithmetic-operators.cpp
@@ -0,0 +1,10 @@
+// User function Template for C++
+
+void operations(int x, int y) {
+        int p =x+y;
+        int q =x-y;
+        int r =x*y;
+        int s =x/y;
+        int t =x%y;
+        cout << p << " " << q << " " << r << " " << s << " " << t << endl;
+}
