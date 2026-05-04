7dfe43cbeaa4c9a2b356697675a8fcc411236cb1
Rohit Kumar Tiwari <rohittiwari0403@gmail.com>
2026-05-04 23:42:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Bitwise Operators/bitwise-operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int d=a^a;
+   int  e=c^b;
+    int f=a&b;
+   int  g=c|(a^a);
+     e=~e;
+    cout<<d<<" "<<e<<" "<<f<<" "<<g<<endl;
+
+    return 0;
+}
